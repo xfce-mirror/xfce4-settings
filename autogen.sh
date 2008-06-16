2c15739e33c631cdecf0c59f653fef7628067b0d
Brian Tarricone <brian@tarricone.org>
2008-06-16 04:58:44 +0000

    +x for autogen, better way to get svn rev from git-svn
    (Old svn revision: 27072)

--- autogen.sh
+++ autogen.sh
@@ -35,7 +35,7 @@ echo >>configure.ac
 # substitute revision and linguas
 linguas=$(sed -e '/^#/d' po/LINGUAS)
 if [ -d .git/svn ]; then
-    revision=$(git-svn log | head -n 2 | tail -n 1 | cut -d' ' -f1 | cut -dr -f2)
+    revision=$(git-svn find-rev trunk)
 elif [ -d .svn ]; then
     revision=$(LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}')
 else
