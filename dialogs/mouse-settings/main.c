4cfa5a91197aaba54fa7b46e2ce218975aa8417a
Stephan Arts <stephan@xfce.org>
2008-06-29 21:49:32 +0000

    Fix credits
    (Old svn revision: 27187)

--- dialogs/mouse-settings/main.c
+++ dialogs/mouse-settings/main.c
@@ -1,6 +1,7 @@
 /*
  *  Copyright (c) 2008 Stephan Arts <stephan@xfce.org>
- *  Copyright (c) 2008 Nick Schermer <nick"xfce.org>
+ *  Copyright (c) 2008 Nick Schermer <nick@xfce.org>
+ *  Copyright (c) 2008 Mike Massonnet <mmassonnet@xfce.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
