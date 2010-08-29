4065258afef8a6b536ca3304bb5da4d153673145
Jérôme Guelfucci <jeromeg@xfce.org>
2010-08-29 02:06:39 +0200

    No startup notification for embedded dialogs (Bug #4990).

--- xfce4-settings-manager/xfce-settings-manager-dialog.c
+++ xfce4-settings-manager/xfce-settings-manager-dialog.c
@@ -587,7 +587,7 @@ xfce_settings_manager_dialog_item_activated(ExoIconView *iconview,
 
         /* Try to spawn the dialog */
         if(!xfce_spawn_command_line_on_screen(gtk_widget_get_screen(GTK_WIDGET(iconview)),
-                                              command, FALSE, snotify, &error))
+                                              command, FALSE, FALSE, &error))
         {
             /* Spawning failed, go back to the overview */
             xfce_settings_manager_dialog_recreate_socket(dialog);
