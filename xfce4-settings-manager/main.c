311aeb6b10245ee7d7847df12ba5f17e3c20df54
Theo Linkspfeifer <lastonestanding@tutanota.com>
2022-07-01 19:26:53 +0000

    dialogs: Revert type hint changes
    See: https://gitlab.xfce.org/xfce/libxfce4ui/-/issues/26#note_15913

--- xfce4-settings-manager/main.c
+++ xfce4-settings-manager/main.c
@@ -100,7 +100,6 @@ main(int argc,
     garcon_set_environment ("XFCE");
 
     dialog = xfce_settings_manager_dialog_new ();
-    gtk_window_set_type_hint (GTK_WINDOW (dialog), GDK_WINDOW_TYPE_HINT_NORMAL);
     gtk_window_present (GTK_WINDOW (dialog));
 
     if (opt_dialog != NULL
