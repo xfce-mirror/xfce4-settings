69295056f8eb3aac25cf00c7197bf15dd0780a8b
Theo Linkspfeifer <lastonestanding@tutanota.com>
2022-11-25 12:20:17 +0100

    appearance: Fix mem leaks

--- dialogs/appearance-settings/main.c
+++ dialogs/appearance-settings/main.c
@@ -807,7 +807,9 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         gsettings = g_settings_new (gsettings_category_gnome_interface);
         if (gsettings)
         {
-            g_settings_set_string (gsettings, "gtk-theme", xfconf_channel_get_string (channel, property_name, NULL));
+            str = xfconf_channel_get_string (channel, property_name, NULL);
+            g_settings_set_string (gsettings, "gtk-theme", str);
+            g_free (str);
         }
     }
     else if (strcmp (property_name, "/Net/IconThemeName") == 0)
@@ -855,7 +857,9 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         gsettings = g_settings_new (gsettings_category_gnome_interface);
         if (gsettings)
         {
-            g_settings_set_string (gsettings, "icon-theme", xfconf_channel_get_string (channel, property_name, NULL));
+            str = xfconf_channel_get_string (channel, property_name, NULL);
+            g_settings_set_string (gsettings, "icon-theme", str);
+            g_free (str);
         }
     }
     else if (strcmp (property_name, "/Gtk/FontName") == 0)
@@ -864,7 +868,9 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         gsettings = g_settings_new (gsettings_category_gnome_interface);
         if (gsettings)
         {
-            g_settings_set_string (gsettings, "font-name", xfconf_channel_get_string (channel, property_name, NULL));
+            str = xfconf_channel_get_string (channel, property_name, NULL);
+            g_settings_set_string (gsettings, "font-name", str);
+            g_free (str);
         }
     }
     else if (strcmp (property_name, "/Gtk/MonospaceFontName") == 0)
@@ -873,7 +879,9 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         gsettings = g_settings_new (gsettings_category_gnome_interface);
         if (gsettings)
         {
-            g_settings_set_string (gsettings, "monospace-font-name", xfconf_channel_get_string (channel, property_name, NULL));
+            str = xfconf_channel_get_string (channel, property_name, NULL);
+            g_settings_set_string (gsettings, "monospace-font-name", str);
+            g_free (str);
         }
     }
 }
