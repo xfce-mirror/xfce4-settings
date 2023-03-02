xfce-mirror/xfce4-settings

commit 9bc9b7251812c1f9e52bf6f3683702ef6a4deeed

appearance-install-theme: Check for theme package validity
It seems that index.theme is mandatory, although there doesn't seem to
be any real specification about it. In any case it seems better not to
blindly install the archive contents.

dialogs/appearance-settings/main.c
@@ -1004,6 +1004,11 @@ install_theme (GtkWidget *widget, gchar **uris, GtkBuilder *builder)
                         _("Unknown format, only archives and directories are supported"));
                     break;
 
+                case 6:
+                    g_set_error_literal (&error, G_SPAWN_ERROR, 0,
+                        _("Not a valid theme package, nothing installed"));
+                    break;
+
                 default:
                     g_set_error (&error, G_SPAWN_ERROR,
                         0, _("An unknown error, exit code is %d"), WEXITSTATUS (status));

dialogs/appearance-settings/appearance-install-theme
@@ -24,6 +24,7 @@
 # 3: Failed to create temp directory
 # 4: Failed to extract archive
 # 5: Unknown file format
+# 6: Not a valid theme package
 # theme-package can be a tar-file zip-file or a directory
 # the following themes types are supported:
 #  - Icon themes (detected by the content of index.theme files found)
@@ -140,4 +141,14 @@ unset -v IFS
 test "$is_icon_theme" -eq 0 || exit 0
 
 # Not an icon theme, so treat as style theme
-mkdir -p "$HOME/.themes" && mv "$tmpdir/"* "$HOME/.themes"
+
+# likely: archive contains a unique root dir and an index.theme file in it
+if test -d "$(echo "$tmpdir/"*)" && test -f "$tmpdir/"*"/index.theme"; then
+    mkdir -p "$HOME/.themes" && mv "$tmpdir/"* "$HOME/.themes"
+# unlikely: no root dir but still a root index.theme file
+elif test -f "$tmpdir/index.theme"; then
+    mkdir -p "$HOME/.themes/$pkgname" && mv "$tmpdir/"* "$HOME/.themes/$pkgname"
+# not a valid theme package
+else
+    exit 6
+fi

commit f2819d4a2bdccc82f1eb20800afba073f15a778f

appearance-install-theme: Fix error message
Redundant with main message.

Fixes: 9bc9b7251812c1f9e52bf6f3683702ef6a4deeed
Related: !53

dialogs/appearance-settings/main.c
@@ -1006,7 +1006,7 @@ install_theme (GtkWidget *widget, gchar **uris, GtkBuilder *builder)
 
                 case 6:
                     g_set_error_literal (&error, G_SPAWN_ERROR, 0,
-                        _("Not a valid theme package, nothing installed"));
+                        _("Not a valid theme package"));
                     break;
 
                 default: