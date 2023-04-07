96f564a4713f5f540d9b63896f2e8b411d12b3bb
Gaël Bonithon <gael@xfce.org>
2023-04-07 16:18:53 +0200

    xfsettingsd: Add comment about GTK 3.24.38 required for gtk-modules

--- xfsettingsd/gtk-settings.c
+++ xfsettingsd/gtk-settings.c
@@ -209,7 +209,7 @@ xfce_gtk_settings_helper_init (XfceGtkSettingsHelper *helper)
     GHashTableIter iter;
     XfconfData *data;
 
-    /* synchronization via gtk-modules */
+    /* synchronization via gtk-modules: requires GTK 3.24.38 to work properly (see MR !104) */
     if (GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ()))
         g_bus_own_name (G_BUS_TYPE_SESSION, "org.gtk.Settings", G_BUS_NAME_OWNER_FLAGS_NONE,
                         bus_acquired, NULL, name_lost, g_object_ref (helper), NULL);
