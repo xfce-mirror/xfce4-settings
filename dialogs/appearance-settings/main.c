xfce-mirror/xfce4-settings

commit b82f0f5605b5334962098a39ee9e73bbc7b99c2c

appearance: Sync color scheme to gsettings (Fixes #406)
The setting is used to enable dark mode for GTK4 applications.

dialogs/appearance-settings/main.c
@@ -809,6 +809,13 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         {
             str = xfconf_channel_get_string (channel, property_name, NULL);
             g_settings_set_string (gsettings, "gtk-theme", str);
+
+            /* Also set the preferred color scheme (needed for GTK4) */
+            if (str != NULL && g_str_has_suffix (str, "-dark"))
+                g_settings_set_string (gsettings, "color-scheme", "prefer-dark");
+            else
+                g_settings_reset (gsettings, "color-scheme");
+
             g_free (str);
         }
     }

commit 4701fa10a77f48d3b9c22e64bef586af7f459bf0

appearance: Check for presence of color-scheme key
Closes: #489
Fixes: b82f0f5605b5334962098a39ee9e73bbc7b99c2c
Related: !90

dialogs/appearance-settings/main.c
@@ -845,13 +845,18 @@ appearance_settings_dialog_channel_property_changed (XfconfChannel *channel,
         /* Set the preferred color scheme (needed for GTK4) */
         if (desktop_interface_gsettings != NULL)
         {
-            str = xfconf_channel_get_string (channel, property_name, NULL);
-            if (str != NULL && g_str_has_suffix (str, "-dark"))
-                g_settings_set_string (desktop_interface_gsettings, "color-scheme", "prefer-dark");
-            else
-                g_settings_reset (desktop_interface_gsettings, "color-scheme");
-
-            g_free (str);
+            GSettingsSchema *schema;
+            g_object_get (desktop_interface_gsettings, "settings-schema", &schema, NULL);
+            if (g_settings_schema_has_key (schema, "color-scheme"))
+              {
+                str = xfconf_channel_get_string (channel, property_name, NULL);
+                if (str != NULL && g_str_has_suffix (str, "-dark"))
+                    g_settings_set_string (desktop_interface_gsettings, "color-scheme", "prefer-dark");
+                else
+                    g_settings_reset (desktop_interface_gsettings, "color-scheme");
+                g_free (str);
+              }
+            g_settings_schema_unref (schema);
         }
     }
     else if (strcmp (property_name, "/Net/IconThemeName") == 0)