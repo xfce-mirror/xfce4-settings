xfce-mirror/xfce4-settings

commit 138226c63ef6155cf8c6fdbcb99ffcfdaa784603

build: Restrict XfceSMClient to X11
Libxfce4ui >= 4.19.4 can be built without X11 support and therefore
whithout XfceSMClient.

xfsettingsd/main.c
@@ -83,13 +83,13 @@ static guint owner_id;
 
 struct t_data_set
 {
-    XfceSMClient         *sm_client;
     GObject              *gtk_decorations_helper;
     GObject              *gtk_settings_helper;
 #ifdef ENABLE_DISPLAY_SETTINGS
     GObject              *displays_helper;
 #endif
 #ifdef ENABLE_X11
+    XfceSMClient         *sm_client;
     GObject              *pointer_helper;
     GObject              *keyboards_helper;
     GObject              *accessibility_helper;
@@ -128,7 +128,6 @@ on_name_acquired (GDBusConnection *connection,
 {
     GBusNameOwnerFlags         dbus_flags;
     struct t_data_set         *s_data;
-    GError                    *error = NULL;
 
     s_data = (struct t_data_set*) user_data;
 
@@ -157,29 +156,33 @@ on_name_acquired (GDBusConnection *connection,
     s_data->displays_helper = xfce_displays_helper_new ();
 #endif
 
+#ifdef ENABLE_X11
     /* connect to session always, even if we quit below.  this way the
      * session manager won't wait for us to time out. */
-    s_data->sm_client = xfce_sm_client_get ();
-    xfce_sm_client_set_restart_style (s_data->sm_client, XFCE_SM_CLIENT_RESTART_IMMEDIATELY);
-    xfce_sm_client_set_desktop_file (s_data->sm_client, XFSETTINGS_DESKTOP_FILE);
-    xfce_sm_client_set_priority (s_data->sm_client, 20);
-    g_signal_connect (G_OBJECT (s_data->sm_client), "quit", G_CALLBACK (gtk_main_quit), NULL);
-    if (!xfce_sm_client_connect (s_data->sm_client, &error) && error)
+    if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
     {
-        g_printerr ("Failed to connect to session manager: %s\n", error->message);
-        g_clear_error (&error);
-    }
-
-#ifdef ENABLE_X11
-    if (g_getenv ("XFSETTINGSD_NO_CLIPBOARD") == NULL && GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
-    {
-        s_data->clipboard_daemon = g_object_new (GSD_TYPE_CLIPBOARD_MANAGER, NULL);
-        if (!gsd_clipboard_manager_start (GSD_CLIPBOARD_MANAGER (s_data->clipboard_daemon), opt_replace))
+        GError *error = NULL;
+        s_data->sm_client = xfce_sm_client_get ();
+        xfce_sm_client_set_restart_style (s_data->sm_client, XFCE_SM_CLIENT_RESTART_IMMEDIATELY);
+        xfce_sm_client_set_desktop_file (s_data->sm_client, XFSETTINGS_DESKTOP_FILE);
+        xfce_sm_client_set_priority (s_data->sm_client, 20);
+        g_signal_connect (G_OBJECT (s_data->sm_client), "quit", G_CALLBACK (gtk_main_quit), NULL);
+        if (!xfce_sm_client_connect (s_data->sm_client, &error) && error)
         {
-            UNREF_GOBJECT (G_OBJECT (s_data->clipboard_daemon));
-            s_data->clipboard_daemon = NULL;
+            g_warning ("Failed to connect to session manager: %s", error->message);
+            g_clear_error (&error);
+        }
 
-            g_printerr (G_LOG_DOMAIN ": %s\n", "Another clipboard manager is already running.");
+        if (g_getenv ("XFSETTINGSD_NO_CLIPBOARD") == NULL)
+        {
+            s_data->clipboard_daemon = g_object_new (GSD_TYPE_CLIPBOARD_MANAGER, NULL);
+            if (!gsd_clipboard_manager_start (GSD_CLIPBOARD_MANAGER (s_data->clipboard_daemon), opt_replace))
+            {
+                UNREF_GOBJECT (G_OBJECT (s_data->clipboard_daemon));
+                s_data->clipboard_daemon = NULL;
+
+                g_warning ("Another clipboard manager is already running.");
+            }
         }
     }
 #endif
@@ -244,7 +247,10 @@ main (gint argc, gchar **argv)
        before we have a chance to fork.
        g_option_context_add_group (context, gtk_get_option_group (FALSE));
     */
-    g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
+#ifdef ENABLE_X11
+    if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
+      g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
+#endif
     g_option_context_set_ignore_unknown_options(context, TRUE);
 
     /* parse options */
@@ -393,7 +399,9 @@ main (gint argc, gchar **argv)
 
     xfconf_shutdown ();
 
+#ifdef ENABLE_X11
     UNREF_GOBJECT (s_data.sm_client);
+#endif
 
     /* release the dbus name */
     if (dbus_connection != NULL)

commit 3e1d08fc13d44adf22437f182c2749bb0079a04b

xfsettingsd: Always add sm-client option group
It's too early to determine the windowing system at this stage, as we
haven't yet called gtk_init(). And if we do call it, we can no longer
demonize below, as was mentioned in a comment about
gtk_get_option_group(). So the simplest thing to do is to always add
this option group (provided you're compiling with X11 support), which
activates very little code in libxfce4ui and nothing specific to X11.

Fixes: 138226c63ef6155cf8c6fdbcb99ffcfdaa784603

xfsettingsd/main.c
@@ -248,8 +248,7 @@ main (gint argc, gchar **argv)
        g_option_context_add_group (context, gtk_get_option_group (FALSE));
     */
 #ifdef ENABLE_X11
-    if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
-      g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
+    g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
 #endif
     g_option_context_set_ignore_unknown_options(context, TRUE);
 