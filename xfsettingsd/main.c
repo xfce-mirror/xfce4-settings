xfce-mirror/xfce4-settings

commit 2367177416990a8280f8027e019520b22c34b327

Move X11 clipboard manager to libxfce4ui
To share it with xfce4-clipman-pugin.

Related: libxfce4ui!115

xfsettingsd/main.c
@@ -64,7 +64,6 @@
 #include "keyboard-layout.h"
 #include "keyboard-shortcuts.h"
 #include "workspaces.h"
-#include "clipboard-manager.h"
 #include "xsettings.h"
 #endif
 
@@ -175,12 +174,9 @@ on_name_acquired (GDBusConnection *connection,
 
         if (g_getenv ("XFSETTINGSD_NO_CLIPBOARD") == NULL)
         {
-            s_data->clipboard_daemon = g_object_new (GSD_TYPE_CLIPBOARD_MANAGER, NULL);
-            if (!gsd_clipboard_manager_start (GSD_CLIPBOARD_MANAGER (s_data->clipboard_daemon), opt_replace))
+            s_data->clipboard_daemon = xfce_clipboard_manager_new (opt_replace);
+            if (s_data->clipboard_daemon == NULL)
             {
-                UNREF_GOBJECT (G_OBJECT (s_data->clipboard_daemon));
-                s_data->clipboard_daemon = NULL;
-
                 g_warning ("Another clipboard manager is already running.");
             }
         }
@@ -384,11 +380,7 @@ main (gint argc, gchar **argv)
     UNREF_GOBJECT (s_data.shortcuts_helper);
     UNREF_GOBJECT (s_data.keyboard_layout_helper);
     UNREF_GOBJECT (s_data.workspaces_helper);
-    if (G_LIKELY (s_data.clipboard_daemon != NULL))
-    {
-        gsd_clipboard_manager_stop (GSD_CLIPBOARD_MANAGER (s_data.clipboard_daemon));
-        UNREF_GOBJECT (s_data.clipboard_daemon);
-    }
+    UNREF_GOBJECT (s_data.clipboard_daemon);
 #endif
     UNREF_GOBJECT (s_data.gtk_decorations_helper);
     UNREF_GOBJECT (s_data.gtk_settings_helper);

configure.ac
@@ -17,7 +17,7 @@ m4_define([gtk_min_version], [3.24.0])
 m4_define([garcon_min_version], [0.1.10])
 m4_define([exo_min_version], [0.11.0])
 m4_define([libxfce4util_min_version], [4.17.2])
-m4_define([libxfce4ui_min_version], [4.17.0])
+m4_define([libxfce4ui_min_version], [4.19.5])
 m4_define([xfconf_min_version], [4.13.0])
 
 m4_define([x11_minimum_version], [1.6.7])

xfsettingsd/Makefile.am
@@ -62,8 +62,6 @@ if ENABLE_X11
 xfsettingsd_SOURCES += \
 	accessibility.c \
 	accessibility.h \
-	clipboard-manager.c \
-	clipboard-manager.h \
 	keyboards.c \
 	keyboards.h \
 	keyboard-shortcuts.c \

xfsettingsd/clipboard-manager.h
@@ -1,59 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
-
-#ifndef __GSD_CLIPBOARD_MANAGER_H
-#define __GSD_CLIPBOARD_MANAGER_H
-
-#include <glib-object.h>
-
-G_BEGIN_DECLS
-
-typedef struct _GsdClipboardManager        GsdClipboardManager;
-typedef struct _GsdClipboardManagerClass   GsdClipboardManagerClass;
-typedef struct _GsdClipboardManagerPrivate GsdClipboardManagerPrivate;
-
-#define GSD_TYPE_CLIPBOARD_MANAGER         (gsd_clipboard_manager_get_type ())
-#define GSD_CLIPBOARD_MANAGER(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GSD_TYPE_CLIPBOARD_MANAGER, GsdClipboardManager))
-#define GSD_CLIPBOARD_MANAGER_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GSD_TYPE_CLIPBOARD_MANAGER, GsdClipboardManagerClass))
-#define GSD_IS_CLIPBOARD_MANAGER(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GSD_TYPE_CLIPBOARD_MANAGER))
-#define GSD_IS_CLIPBOARD_MANAGER_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GSD_TYPE_CLIPBOARD_MANAGER))
-#define GSD_CLIPBOARD_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GSD_TYPE_CLIPBOARD_MANAGER, GsdClipboardManagerClass))
-
-struct _GsdClipboardManager
-{
-    GObject                     parent;
-    GsdClipboardManagerPrivate *priv;
-};
-
-struct _GsdClipboardManagerClass
-{
-    GObjectClass parent_class;
-};
-
-GType gsd_clipboard_manager_get_type (void);
-
-gboolean gsd_clipboard_manager_start (GsdClipboardManager *manager,
-                                      gboolean             replace);
-
-void     gsd_clipboard_manager_stop  (GsdClipboardManager *manager);
-
-G_END_DECLS
-
-#endif /* __GSD_CLIPBOARD_MANAGER_H */

commit 34c075139e2dc2804034eaac54762282a0660842

xfsettingsd: Fix -Wincompatible-pointer-types warning
Fixes: 2367177416990a8280f8027e019520b22c34b327

xfsettingsd/main.c
@@ -174,7 +174,7 @@ on_name_acquired (GDBusConnection *connection,
 
         if (g_getenv ("XFSETTINGSD_NO_CLIPBOARD") == NULL)
         {
-            s_data->clipboard_daemon = xfce_clipboard_manager_new (opt_replace);
+            s_data->clipboard_daemon = G_OBJECT (xfce_clipboard_manager_new (opt_replace));
             if (s_data->clipboard_daemon == NULL)
             {
                 g_warning ("Another clipboard manager is already running.");