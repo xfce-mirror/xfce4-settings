xfce-mirror/xfce4-settings

commit 332ad93dd65eff6128b0a3a119085744e0b78cb5

display-settings: Fix ellipsizing and spacing in minimal dialog

dialogs/display-settings/main.c
@@ -1140,6 +1140,10 @@ display_settings_minimal_profile_populate (XfceDisplaySettings *settings)
         profile_name = xfconf_channel_get_string (channel, property, NULL);
 
         label = gtk_label_new (profile_name);
+        gtk_widget_set_tooltip_text (label, profile_name);
+        gtk_widget_set_size_request (label, 128, -1);
+        gtk_label_set_ellipsize (GTK_LABEL (label), PANGO_ELLIPSIZE_MIDDLE);
+        gtk_label_set_max_width_chars (GTK_LABEL (label), 0);
         image = gtk_image_new_from_icon_name ("xfce-display-profile", 128);
         gtk_image_set_pixel_size (GTK_IMAGE (image), 128);
 
@@ -1148,11 +1152,11 @@ display_settings_minimal_profile_populate (XfceDisplaySettings *settings)
         g_object_set_data_full (G_OBJECT (profile_radio), "profile", current->data, g_free);
         gtk_toggle_button_set_mode (GTK_TOGGLE_BUTTON (profile_radio), FALSE);
         gtk_widget_set_size_request (GTK_WIDGET (profile_radio), 128, 128);
+        gtk_widget_set_halign (GTK_WIDGET (profile_radio), GTK_ALIGN_CENTER);
 
         box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
         gtk_box_pack_start (GTK_BOX (box), profile_radio, FALSE, TRUE, 0);
         gtk_box_pack_start (GTK_BOX (box), label, FALSE, TRUE, 3);
-        gtk_widget_set_margin_start (GTK_WIDGET (box), 24);
         gtk_box_pack_start (GTK_BOX (profile_box), box, FALSE, FALSE, 0);
 
         g_signal_connect (profile_radio, "toggled", G_CALLBACK (display_settings_minimal_profile_apply), channel);

dialogs/display-settings/minimal-display-dialog.glade
@@ -27,7 +27,8 @@
           <object class="GtkGrid" id="buttons">
             <property name="visible">True</property>
             <property name="can_focus">False</property>
-            <property name="row_spacing">3</property>
+            <property name="row_spacing">6</property>
+            <property name="column_spacing">24</property>
             <child>
               <object class="GtkBox" id="profile-box">
                 <property name="visible">True</property>
@@ -74,8 +75,10 @@
                     <property name="width_request">128</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
+                    <property name="tooltip_text" translatable="yes">Only Display 1</property>
                     <property name="label" translatable="yes">Only Display 1</property>
                     <property name="ellipsize">middle</property>
+                    <property name="max-width-chars">0</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
@@ -93,7 +96,6 @@
               <object class="GtkBox">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
-                <property name="margin_left">24</property>
                 <property name="hexpand">True</property>
                 <property name="orientation">vertical</property>
                 <property name="spacing">3</property>
@@ -129,6 +131,7 @@
                     <property name="tooltip_text" translatable="yes">Mirror Displays</property>
                     <property name="label" translatable="yes">Mirror Displays</property>
                     <property name="ellipsize">middle</property>
+                    <property name="max-width-chars">0</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
@@ -146,7 +149,6 @@
               <object class="GtkBox">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
-                <property name="margin_left">24</property>
                 <property name="hexpand">True</property>
                 <property name="orientation">vertical</property>
                 <property name="spacing">3</property>
@@ -182,6 +184,7 @@
                     <property name="tooltip_text" translatable="yes">Extend to the right</property>
                     <property name="label" translatable="yes">Extend to the right</property>
                     <property name="ellipsize">middle</property>
+                    <property name="max-width-chars">0</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
@@ -199,7 +202,6 @@
               <object class="GtkBox">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
-                <property name="margin_left">24</property>
                 <property name="hexpand">True</property>
                 <property name="orientation">vertical</property>
                 <property name="spacing">3</property>
@@ -232,8 +234,10 @@
                     <property name="width_request">128</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
+                    <property name="tooltip_text" translatable="yes">Only Display 2</property>
                     <property name="label" translatable="yes">Only Display 2</property>
                     <property name="ellipsize">middle</property>
+                    <property name="max-width-chars">0</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>

commit 41ca1b1b7c9479a9be93a8bf9cf77dc4109c37de

display-settings: Remove right margin in minimal dialog without profile
Fixes: 332ad93dd65eff6128b0a3a119085744e0b78cb5

dialogs/display-settings/main.c
@@ -1172,6 +1172,7 @@ display_settings_minimal_profile_populate (XfceDisplaySettings *settings)
     }
 
     gtk_widget_show_all (GTK_WIDGET (profile_box));
+    gtk_widget_set_visible (GTK_WIDGET (profile_box), profiles != NULL);
     g_list_free (profiles);
     g_free (active_profile);
 }