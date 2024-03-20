xfce-mirror/xfce4-settings

commit 2962eb460e24ce31fcb91b41149ab20991ea835f

display-settings: Turn "auto-enable-profiles" into a combo box
Hopefully this will be less confusing:
* Automatic activation of a profile is exclusive of the actions above
  only if it succeeds, otherwise we continue.
* It can now only take place when a display is connected or
  disconnected, or always or never as before.

Backward compatibility is almost complete automatically, so there's
probably no need to complicate things.

Replaces: 8fd93e6561a54c1d0edb956aea67966ce3088965

dialogs/display-settings/main.c
@@ -1641,25 +1641,6 @@ display_settings_profile_delete (GtkWidget *widget, XfceDisplaySettings *setting
 }
 
 static void
-display_setting_minimal_autoconnect_mode_changed (GtkComboBox *combobox,
-                                                  GtkBuilder  *builder)
-{
-    gint value;
-    gboolean state = TRUE;
-    GObject *auto_enable_profiles;
-
-    value = gtk_combo_box_get_active (combobox);
-    /* On "Do nothing" disable the "auto-enable-profiles" option */
-    if (value == 0)
-      state = FALSE;
-
-    auto_enable_profiles = gtk_builder_get_object (builder, "auto-enable-profiles");
-    gtk_widget_set_sensitive (GTK_WIDGET (auto_enable_profiles), state);
-    auto_enable_profiles = gtk_builder_get_object (builder, "auto-enable-profiles-label");
-    gtk_widget_set_sensitive (GTK_WIDGET (auto_enable_profiles), state);
-}
-
-static void
 display_settings_launch_settings_dialogs (GtkButton *button,
                                           gpointer   user_data)
 {
@@ -1853,23 +1834,19 @@ display_settings_dialog_new (XfceDisplaySettings *settings)
     g_signal_connect (G_OBJECT (combobox), "row-activated", G_CALLBACK (display_settings_profile_row_activated), settings);
 
     combobox = gtk_builder_get_object (builder, "autoconnect-mode");
-    g_signal_connect (G_OBJECT (combobox), "changed", G_CALLBACK (display_setting_minimal_autoconnect_mode_changed), builder);
-    xfconf_g_property_bind (channel, "/Notify", G_TYPE_INT, combobox,
-                            "active");
+    xfconf_g_property_bind (channel, "/Notify", G_TYPE_INT, combobox, "active");
     /* Correctly initialize the state of the auto-enable-profiles setting based on autoconnect-mode */
     if (xfconf_channel_get_int (channel, "/Notify", -1) == -1)
     {
         gtk_combo_box_set_active (GTK_COMBO_BOX (combobox), NOTIFY_PROP_DEFAULT);
-        display_setting_minimal_autoconnect_mode_changed ((GTK_COMBO_BOX (combobox)), builder);
     }
 
     apply_button = GTK_WIDGET (gtk_builder_get_object (builder, "apply"));
     g_signal_connect (G_OBJECT (apply_button), "clicked", G_CALLBACK (display_setting_apply), settings);
     gtk_widget_set_sensitive (apply_button, FALSE);
 
-    check = gtk_builder_get_object (builder, "auto-enable-profiles");
-    xfconf_g_property_bind (channel, "/AutoEnableProfiles", G_TYPE_BOOLEAN, check,
-                            "active");
+    combobox = gtk_builder_get_object (builder, "auto-enable-profiles");
+    xfconf_g_property_bind (channel, "/AutoEnableProfiles", G_TYPE_INT, combobox, "active");
 
     button = GTK_WIDGET (gtk_builder_get_object (builder, "button-profile-save"));
     gtk_widget_set_sensitive (button, FALSE);

xfsettingsd/displays-wayland.c
@@ -449,12 +449,18 @@ manager_listener (XfceWlrOutputManager *manager,
        apply it if there's only one */
     if (outputs->len != helper->previous_n_outputs)
     {
-        const gchar *matching_profile = xfce_displays_helper_get_matching_profile (XFCE_DISPLAYS_HELPER (helper));
-        if (matching_profile != NULL)
+        gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+        if (mode == AUTO_ENABLE_PROFILES_ALWAYS
+            || (mode == AUTO_ENABLE_PROFILES_ON_CONNECT && outputs->len > helper->previous_n_outputs)
+            || (mode == AUTO_ENABLE_PROFILES_ON_DISCONNECT && outputs->len < helper->previous_n_outputs))
         {
-            xfce_displays_helper_wayland_channel_apply (XFCE_DISPLAYS_HELPER (helper), matching_profile);
-            helper->previous_n_outputs = outputs->len;
-            return;
+            const gchar *matching_profile = xfce_displays_helper_get_matching_profile (XFCE_DISPLAYS_HELPER (helper));
+            if (matching_profile != NULL)
+            {
+                xfce_displays_helper_wayland_channel_apply (XFCE_DISPLAYS_HELPER (helper), matching_profile);
+                helper->previous_n_outputs = outputs->len;
+                return;
+            }
         }
         xfconf_channel_set_string (channel, ACTIVE_PROFILE, DEFAULT_SCHEME_NAME);
     }

xfsettingsd/displays-x11.c
@@ -562,14 +562,19 @@ xfce_displays_helper_x11_screen_on_event (GdkXEvent *xevent,
 
         /* Check if we have different amount of outputs and a matching profile and
            apply it if there's only one */
-        if (old_outputs->len > helper->outputs->len ||
-            old_outputs->len < helper->outputs->len)
+        if (helper->outputs->len != old_outputs->len)
         {
-            const gchar *matching_profile = xfce_displays_helper_get_matching_profile (XFCE_DISPLAYS_HELPER (helper));
-            if (matching_profile != NULL)
+            gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+            if (mode == AUTO_ENABLE_PROFILES_ALWAYS
+                || (mode == AUTO_ENABLE_PROFILES_ON_CONNECT && helper->outputs->len > old_outputs->len)
+                || (mode == AUTO_ENABLE_PROFILES_ON_DISCONNECT && helper->outputs->len < old_outputs->len))
             {
-                xfce_displays_helper_x11_channel_apply (XFCE_DISPLAYS_HELPER (helper), matching_profile);
-                return GDK_FILTER_CONTINUE;
+                const gchar *matching_profile = xfce_displays_helper_get_matching_profile (XFCE_DISPLAYS_HELPER (helper));
+                if (matching_profile != NULL)
+                {
+                    xfce_displays_helper_x11_channel_apply (XFCE_DISPLAYS_HELPER (helper), matching_profile);
+                    return GDK_FILTER_CONTINUE;
+                }
             }
             xfconf_channel_set_string (channel, ACTIVE_PROFILE, DEFAULT_SCHEME_NAME);
         }

xfsettingsd/displays.c
@@ -112,6 +112,7 @@ xfce_displays_helper_constructed (GObject *object)
     if (XFCE_DISPLAYS_HELPER_GET_CLASS (helper)->get_outputs (helper) != NULL)
     {
         const gchar *matching_profile;
+        gint mode;
 
 #ifdef HAVE_UPOWERGLIB
         priv->power = g_object_new (XFCE_TYPE_DISPLAYS_UPOWER, NULL);
@@ -135,7 +136,8 @@ xfce_displays_helper_constructed (GObject *object)
 
         /*  check if we can auto-enable a profile */
         matching_profile = xfce_displays_helper_get_matching_profile (helper);
-        if (matching_profile != NULL)
+        mode = xfconf_channel_get_int (priv->channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+        if (matching_profile != NULL && (mode == AUTO_ENABLE_PROFILES_ON_CONNECT || mode == AUTO_ENABLE_PROFILES_ALWAYS))
         {
             XFCE_DISPLAYS_HELPER_GET_CLASS (helper)->channel_apply (helper, matching_profile);
         }
@@ -188,13 +190,8 @@ xfce_displays_helper_get_matching_profile (XfceDisplaysHelper *helper)
 {
     XfceDisplaysHelperPrivate *priv = get_instance_private (helper);
     GList *profiles = NULL;
-    gchar **display_infos;
+    gchar **display_infos = XFCE_DISPLAYS_HELPER_GET_CLASS (helper)->get_display_infos (helper);
 
-    if (!xfconf_channel_get_bool (priv->channel, AUTO_ENABLE_PROFILES, FALSE)
-        || xfconf_channel_get_int (priv->channel, NOTIFY_PROP, ACTION_ON_NEW_OUTPUT_SHOW_DIALOG) == ACTION_ON_NEW_OUTPUT_DO_NOTHING)
-        return NULL;
-
-    display_infos = XFCE_DISPLAYS_HELPER_GET_CLASS (helper)->get_display_infos (helper);
     if (display_infos != NULL)
     {
         profiles = display_settings_get_profiles (display_infos, priv->channel);

xfsettingsd/displays.h
@@ -65,6 +65,14 @@ enum
     ACTION_ON_NEW_OUTPUT_EXTEND,
 };
 
+enum
+{
+    AUTO_ENABLE_PROFILES_NEVER,
+    AUTO_ENABLE_PROFILES_ON_CONNECT,
+    AUTO_ENABLE_PROFILES_ON_DISCONNECT,
+    AUTO_ENABLE_PROFILES_ALWAYS,
+};
+
 struct _XfceDisplaysHelperClass
 {
     GObjectClass __parent__;

dialogs/display-settings/display-dialog.glade
@@ -44,6 +44,32 @@
     <property name="can-focus">False</property>
     <property name="icon-name">object-select-symbolic</property>
   </object>
+  <object class="GtkListStore" id="liststore1">
+    <columns>
+      <!-- column-name gchararray1 -->
+      <column type="gchararray"/>
+      <!-- column-name guint1 -->
+      <column type="guint"/>
+    </columns>
+    <data>
+      <row>
+        <col id="0" translatable="yes">Never</col>
+        <col id="1">0</col>
+      </row>
+      <row>
+        <col id="0" translatable="yes">When a display is connected</col>
+        <col id="1">1</col>
+      </row>
+      <row>
+        <col id="0" translatable="yes">When a display is disconnected</col>
+        <col id="1">2</col>
+      </row>
+      <row>
+        <col id="0" translatable="yes">Always</col>
+        <col id="1">3</col>
+      </row>
+    </data>
+  </object>
   <object class="GtkListStore" id="liststore2">
     <columns>
       <!-- column-name gchararray1 -->
@@ -802,7 +828,7 @@
                         <property name="visible">True</property>
                         <property name="can-focus">False</property>
                         <property name="margin-left">12</property>
-                        <property name="label" translatable="yes">Automatically enable profiles when new display is connected</property>
+                        <property name="label" translatable="yes">First try to automatically enable a profile:</property>
                         <property name="xalign">0</property>
                       </object>
                       <packing>
@@ -811,11 +837,18 @@
                       </packing>
                     </child>
                     <child>
-                      <object class="GtkSwitch" id="auto-enable-profiles">
+                      <object class="GtkComboBox" id="auto-enable-profiles">
                         <property name="visible">True</property>
-                        <property name="can-focus">True</property>
-                        <property name="halign">end</property>
+                        <property name="can-focus">False</property>
                         <property name="valign">center</property>
+                        <property name="model">liststore1</property>
+                        <property name="id-column">1</property>
+                        <child>
+                          <object class="GtkCellRendererText" id="cellrenderertext1"/>
+                          <attributes>
+                            <attribute name="text">0</attribute>
+                          </attributes>
+                        </child>
                       </object>
                       <packing>
                         <property name="left-attach">1</property>
@@ -827,7 +860,7 @@
                         <property name="visible">True</property>
                         <property name="can-focus">False</property>
                         <property name="margin-left">12</property>
-                        <property name="label" translatable="yes">When new displays are connected:</property>
+                        <property name="label" translatable="yes">When a display is connected:</property>
                         <property name="xalign">0</property>
                       </object>
                       <packing>
@@ -894,27 +927,6 @@
                         <property name="left-attach">1</property>
                         <property name="top-attach">3</property>
                       </packing>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
-                    </child>
-                    <child>
-                      <placeholder/>
                     </child>
                   </object>
                   <packing>

commit 8198a0456b4426fb0946f030b17f7474bdde8108

display-settings: Share default setting values
This will avoid inconsistencies between UI and xfsettingsd. Also fix
missing default value for "auto-enable-profiles" combo box.

Fixes: 2962eb460e24ce31fcb91b41149ab20991ea835f

common/display-profiles.h
@@ -19,6 +19,24 @@
 #include <glib.h>
 #include <xfconf/xfconf.h>
 
+enum
+{
+    ACTION_ON_NEW_OUTPUT_DO_NOTHING,
+    ACTION_ON_NEW_OUTPUT_SHOW_DIALOG,
+    ACTION_ON_NEW_OUTPUT_MIRROR,
+    ACTION_ON_NEW_OUTPUT_EXTEND,
+};
+
+enum
+{
+    AUTO_ENABLE_PROFILES_NEVER,
+    AUTO_ENABLE_PROFILES_ON_CONNECT,
+    AUTO_ENABLE_PROFILES_ON_DISCONNECT,
+    AUTO_ENABLE_PROFILES_ALWAYS,
+};
+
+#define ACTION_ON_NEW_OUTPUT_DEFAULT ACTION_ON_NEW_OUTPUT_SHOW_DIALOG
+#define AUTO_ENABLE_PROFILES_DEFAULT AUTO_ENABLE_PROFILES_NEVER
 
 gboolean display_settings_profile_name_exists   (XfconfChannel  *channel,
                                                  const gchar    *new_profile_name);

dialogs/display-settings/main.c
@@ -59,7 +59,6 @@
 #include "display-settings.h"
 
 #define MARGIN  16
-#define NOTIFY_PROP_DEFAULT 1
 #define ONLY_DISPLAY_1 _("Only %s (1)")
 #define ONLY_DISPLAY_2 _("Only %s (2)")
 
@@ -1847,10 +1846,9 @@ display_settings_dialog_new (XfceDisplaySettings *settings)
 
     combobox = gtk_builder_get_object (builder, "autoconnect-mode");
     xfconf_g_property_bind (channel, "/Notify", G_TYPE_INT, combobox, "active");
-    /* Correctly initialize the state of the auto-enable-profiles setting based on autoconnect-mode */
     if (xfconf_channel_get_int (channel, "/Notify", -1) == -1)
     {
-        gtk_combo_box_set_active (GTK_COMBO_BOX (combobox), NOTIFY_PROP_DEFAULT);
+        gtk_combo_box_set_active (GTK_COMBO_BOX (combobox), ACTION_ON_NEW_OUTPUT_DEFAULT);
     }
 
     apply_button = GTK_WIDGET (gtk_builder_get_object (builder, "apply"));
@@ -1859,6 +1857,10 @@ display_settings_dialog_new (XfceDisplaySettings *settings)
 
     combobox = gtk_builder_get_object (builder, "auto-enable-profiles");
     xfconf_g_property_bind (channel, "/AutoEnableProfiles", G_TYPE_INT, combobox, "active");
+    if (xfconf_channel_get_int (channel, "/AutoEnableProfiles", -1) == -1)
+    {
+        gtk_combo_box_set_active (GTK_COMBO_BOX (combobox), AUTO_ENABLE_PROFILES_DEFAULT);
+    }
 
     button = GTK_WIDGET (gtk_builder_get_object (builder, "button-profile-save"));
     gtk_widget_set_sensitive (button, FALSE);

xfsettingsd/displays-wayland.c
@@ -27,6 +27,7 @@
 #include "common/xfce-wlr-output-manager.h"
 #include "common/debug.h"
 #include "common/edid.h"
+#include "common/display-profiles.h"
 #include "displays-wayland.h"
 
 
@@ -449,7 +450,7 @@ manager_listener (XfceWlrOutputManager *manager,
        apply it if there's only one */
     if (outputs->len != helper->previous_n_outputs)
     {
-        gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+        gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_DEFAULT);
         if (mode == AUTO_ENABLE_PROFILES_ALWAYS
             || (mode == AUTO_ENABLE_PROFILES_ON_CONNECT && outputs->len > helper->previous_n_outputs)
             || (mode == AUTO_ENABLE_PROFILES_ON_DISCONNECT && outputs->len < helper->previous_n_outputs))
@@ -487,7 +488,7 @@ manager_listener (XfceWlrOutputManager *manager,
     }
     else
     {
-        gint action = xfconf_channel_get_int (channel, NOTIFY_PROP, ACTION_ON_NEW_OUTPUT_SHOW_DIALOG);
+        gint action = xfconf_channel_get_int (channel, NOTIFY_PROP, ACTION_ON_NEW_OUTPUT_DEFAULT);
         update_needed = TRUE;
 
         for (guint n = 0; n < outputs->len; n++)

xfsettingsd/displays-x11.c
@@ -564,7 +564,7 @@ xfce_displays_helper_x11_screen_on_event (GdkXEvent *xevent,
            apply it if there's only one */
         if (helper->outputs->len != old_outputs->len)
         {
-            gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+            gint mode = xfconf_channel_get_int (channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_DEFAULT);
             if (mode == AUTO_ENABLE_PROFILES_ALWAYS
                 || (mode == AUTO_ENABLE_PROFILES_ON_CONNECT && helper->outputs->len > old_outputs->len)
                 || (mode == AUTO_ENABLE_PROFILES_ON_DISCONNECT && helper->outputs->len < old_outputs->len))
@@ -627,7 +627,7 @@ xfce_displays_helper_x11_screen_on_event (GdkXEvent *xevent,
         }
         else
         {
-            action = xfconf_channel_get_int (channel, NOTIFY_PROP, ACTION_ON_NEW_OUTPUT_SHOW_DIALOG);
+            action = xfconf_channel_get_int (channel, NOTIFY_PROP, ACTION_ON_NEW_OUTPUT_DEFAULT);
 
             /* Diff the new and old output list to find new outputs */
             for (n = 0; n < helper->outputs->len; ++n)

xfsettingsd/displays.c
@@ -136,7 +136,7 @@ xfce_displays_helper_constructed (GObject *object)
 
         /*  check if we can auto-enable a profile */
         matching_profile = xfce_displays_helper_get_matching_profile (helper);
-        mode = xfconf_channel_get_int (priv->channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_NEVER);
+        mode = xfconf_channel_get_int (priv->channel, AUTO_ENABLE_PROFILES, AUTO_ENABLE_PROFILES_DEFAULT);
         if (matching_profile != NULL && (mode == AUTO_ENABLE_PROFILES_ON_CONNECT || mode == AUTO_ENABLE_PROFILES_ALWAYS))
         {
             XFCE_DISPLAYS_HELPER_GET_CLASS (helper)->channel_apply (helper, matching_profile);

xfsettingsd/displays.h
@@ -57,22 +57,6 @@ G_BEGIN_DECLS
 #define XFCE_TYPE_DISPLAYS_HELPER (xfce_displays_helper_get_type ())
 G_DECLARE_DERIVABLE_TYPE (XfceDisplaysHelper, xfce_displays_helper, XFCE, DISPLAYS_HELPER, GObject)
 
-enum
-{
-    ACTION_ON_NEW_OUTPUT_DO_NOTHING,
-    ACTION_ON_NEW_OUTPUT_SHOW_DIALOG,
-    ACTION_ON_NEW_OUTPUT_MIRROR,
-    ACTION_ON_NEW_OUTPUT_EXTEND,
-};
-
-enum
-{
-    AUTO_ENABLE_PROFILES_NEVER,
-    AUTO_ENABLE_PROFILES_ON_CONNECT,
-    AUTO_ENABLE_PROFILES_ON_DISCONNECT,
-    AUTO_ENABLE_PROFILES_ALWAYS,
-};
-
 struct _XfceDisplaysHelperClass
 {
     GObjectClass __parent__;