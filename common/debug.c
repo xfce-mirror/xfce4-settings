c75e155747fa41033ba0b3ed8c983160e73a9b4d
Gaël Bonithon <gael@xfce.org>
2024-06-04 20:41:32 +0200

    build: clang: Fix -Wignored-attributes

--- common/debug.c
+++ common/debug.c
@@ -63,7 +63,7 @@ xfsettings_dbg_init (void)
 
 
 
-static void __attribute__ ((format (gnu_printf, 2, 0)))
+static void
 xfsettings_dbg_print (XfsdDebugDomain domain,
                       const gchar *message,
                       va_list args)
