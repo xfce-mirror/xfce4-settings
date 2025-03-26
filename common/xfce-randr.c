xfce-mirror/xfce4-settings

commit bcbbb351cf7c8b32b92d1a8c9fd7d8d85aa69d1b

xfce-randr: Take RR_DoubleScan and RR_Interlace flags into account
Fixes: #529
Related: https://gitlab.gnome.org/GNOME/mutter/-/commit/60d7cd82ba7dcaff2f7b45226fe0782a8dbf24e6

common/xfce-randr.c
@@ -110,10 +110,16 @@ xfce_randr_list_supported_modes (XRRScreenResources *resources,
         {
             if (output_info->modes[n] == resources->modes[m].id)
             {
+                gdouble v_total = resources->modes[m].vTotal;
+                if (resources->modes[m].modeFlags & RR_DoubleScan)
+                    v_total *= 2.0;
+                if (resources->modes[m].modeFlags & RR_Interlace)
+                    v_total /= 2.0;
+
                 modes[n].width = resources->modes[m].width;
                 modes[n].height = resources->modes[m].height;
                 modes[n].rate = (gdouble) resources->modes[m].dotClock /
-                                ((gdouble) resources->modes[m].hTotal * (gdouble) resources->modes[m].vTotal);
+                                ((gdouble) resources->modes[m].hTotal * v_total);
 
                 break;
             }

commit a13ccb13af765b2714b110d8a2294b3afa58c2b2

x11: display: Properly take XRRModeFlags into account
Not only in xfce4-display-settings, but also in xfsettingsd.

Fixes: bcbbb351cf7c8b32b92d1a8c9fd7d8d85aa69d1b
Related: #529

common/xfce-randr.c
@@ -105,16 +105,10 @@ xfce_randr_list_supported_modes (XRRScreenResources *resources,
         {
             if (output_info->modes[n] == resources->modes[m].id)
             {
-                gdouble v_total = resources->modes[m].vTotal;
-                if (resources->modes[m].modeFlags & RR_DoubleScan)
-                    v_total *= 2.0;
-                if (resources->modes[m].modeFlags & RR_Interlace)
-                    v_total /= 2.0;
-
                 modes[n].width = resources->modes[m].width;
                 modes[n].height = resources->modes[m].height;
-                modes[n].rate = (gdouble) resources->modes[m].dotClock
-                                / ((gdouble) resources->modes[m].hTotal * v_total);
+                modes[n].rate = xfce_randr_calculate_refresh_rate (resources->modes[m]);
+                modes[n].flags = resources->modes[m].modeFlags;
 
                 break;
             }
@@ -434,6 +428,10 @@ xfce_randr_save_output (XfceRandr *randr,
     g_snprintf (property, sizeof (property), "/%s/%s/RefreshRate", scheme,
                 randr->priv->output_info[output]->name);
     xfconf_channel_set_double (channel, property, mode->rate);
+
+    g_snprintf (property, sizeof (property), "/%s/%s/ModeFlags", scheme,
+                randr->priv->output_info[output]->name);
+    xfconf_channel_set_uint64 (channel, property, mode->flags);
 
     /* convert the rotation into degrees */
     switch (randr->rotation[output] & XFCE_RANDR_ROTATIONS_MASK)
@@ -799,4 +797,18 @@ xfce_randr_mode_height (XfceRandr *randr,
         return round (mode->width * randr->scalex[output]);
     else
         return round (mode->height * randr->scaley[output]);
+}
+
+
+
+gdouble
+xfce_randr_calculate_refresh_rate (XRRModeInfo mode)
+{
+    gdouble v_total = mode.vTotal;
+    if (mode.modeFlags & RR_DoubleScan)
+        v_total *= 2.0;
+    if (mode.modeFlags & RR_Interlace)
+        v_total /= 2.0;
+
+    return mode.dotClock / ((gdouble) mode.hTotal * v_total);
 }

common/xfce-randr.h
@@ -47,6 +47,7 @@ struct _XfceRRMode
     guint width;
     guint height;
     gdouble rate;
+    XRRModeFlags flags;
 };
 
 struct _XfceOutputPosition
@@ -143,6 +144,9 @@ guint
 xfce_randr_mode_height (XfceRandr *randr,
                         guint output,
                         const XfceRRMode *mode);
+
+gdouble
+xfce_randr_calculate_refresh_rate (XRRModeInfo mode);
 
 G_END_DECLS
 

xfsettingsd/displays-x11.c
@@ -801,6 +801,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     gdouble output_rate, rate;
     gdouble scale;
     RRMode valid_mode;
+    XRRModeFlags mode_flags;
     Rotation rot;
     gint x, y, n, m, int_value;
     gboolean active;
@@ -914,6 +915,13 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     else
         output_rate = 0.0;
 
+    g_snprintf (property, sizeof (property), MODE_FLAGS_PROP, scheme, output->info->name);
+    value = g_hash_table_lookup (saved_outputs, property);
+    if (value == NULL || !G_VALUE_HOLDS_UINT64 (value))
+        mode_flags = 0;
+    else
+        mode_flags = g_value_get_uint64 (value);
+
     /* scaling */
     g_snprintf (property, sizeof (property), SCALE_PROP, scheme, output->info->name);
     value = g_hash_table_lookup (saved_outputs, property);
@@ -950,8 +958,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
                 continue;
 
             /* calculate the refresh rate */
-            rate = (gdouble) helper->resources->modes[m].dotClock
-                   / ((gdouble) helper->resources->modes[m].hTotal * (gdouble) helper->resources->modes[m].vTotal);
+            rate = xfce_randr_calculate_refresh_rate (helper->resources->modes[m]);
 
             /* construct a string equivalent to the mode generated in displays */
             /* property is the resources mode translated into display panel name */
@@ -959,7 +966,8 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
                         helper->resources->modes[m].height);
 
             /* find the mode corresponding to the saved values */
-            if (rint (rate * 100) == rint (output_rate * 100)
+            if ((mode_flags == 0 || mode_flags == helper->resources->modes[m].modeFlags)
+                && rint (rate * 100) == rint (output_rate * 100)
                 && (g_strcmp0 (property, str_value) == 0))
             {
                 valid_mode = helper->resources->modes[m].id;

xfsettingsd/displays.h
@@ -37,6 +37,7 @@
 #define SCALE_PROP OUTPUT_FMT "/Scale"
 #define SCALEX_PROP OUTPUT_FMT "/Scale/X"
 #define RRATE_PROP OUTPUT_FMT "/RefreshRate"
+#define MODE_FLAGS_PROP OUTPUT_FMT "/ModeFlags"
 #define POSX_PROP OUTPUT_FMT "/Position/X"
 #define POSY_PROP OUTPUT_FMT "/Position/Y"
 #define NOTIFY_PROP "/Notify"