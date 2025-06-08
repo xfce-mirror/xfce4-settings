f948c1c117e9b9549a68cb3cda556b03d283294f
DictyosteliumDiscoideum <dictyosteliumdiscoideum@protonmail.com>
2025-06-08 02:05:02 -0600

    Add default video player

--- dialogs/mime-settings/xfce-mime-helper-launcher-dialog.c
+++ dialogs/mime-settings/xfce-mime-helper-launcher-dialog.c
@@ -260,6 +260,8 @@ xfce_mime_helper_launcher_dialog_set_category (XfceMimeHelperLauncherDialog *lau
         "Viewer now and click OK to proceed."),
     N_ ("Please choose your preferred Audio\n"
         "Player now and click OK to proceed."),
+    N_ ("Please choose your preferred Video\n"
+        "Player now and click OK to proceed."),
   };
 
   /* sanity check the categories */
