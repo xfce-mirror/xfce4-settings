8fd41f44ef8a767cbe27db22c985662be737ee2c
DictyosteliumDiscoideum <dictyosteliumdiscoideum@protonmail.com>
2025-06-08 02:04:30 -0600

    Add default audio player

--- dialogs/mime-settings/xfce-mime-helper-main.c
+++ dialogs/mime-settings/xfce-mime-helper-main.c
@@ -31,6 +31,7 @@ static const gchar *CATEGORY_EXEC_ERRORS[] = {
   N_ ("Failed to execute default File Manager"),
   N_ ("Failed to execute default Terminal Emulator"),
   N_ ("Failed to execute default Image Viewer"),
+  N_ ("Failed to execute default Audio Player"),
 };
 
 
@@ -131,7 +132,7 @@ main (int argc,
   g_option_context_add_main_entries (opt_ctx, option_entries, NULL);
   g_option_context_set_ignore_unknown_options (opt_ctx, TRUE);
   /* Note to Translators: Do not translate the TYPEs (WebBrowser, MailReader,
-   * FileManager, TerminalEmulator, and ImageViewer),
+   * FileManager, TerminalEmulator, ImageViewer, and AudioPlayer),
    * since the xfce-mime-helper utility will
    * not accept localized TYPEs.
    */
@@ -141,7 +142,8 @@ main (int argc,
                                       "  MailReader       - The preferred Mail Reader.\n"
                                       "  FileManager      - The preferred File Manager.\n"
                                       "  TerminalEmulator - The preferred Terminal Emulator.\n"
-                                      "  ImageViewer      - The preferred Image Viewer.\n"));
+                                      "  ImageViewer      - The preferred Image Viewer.\n"
+                                      "  AudioPlayer      - The preferred Audio Player.\n"));
 
   if (!g_option_context_parse (opt_ctx, &argc, &argv, &error))
     {
