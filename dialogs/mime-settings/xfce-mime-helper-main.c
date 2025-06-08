54230ead2f1d1859864b93b20992c9bead700f69
DictyosteliumDiscoideum <dictyosteliumdiscoideum@protonmail.com>
2025-06-08 02:04:30 -0600

    Add default audio player

--- dialogs/mime-settings/xfce-mime-helper-main.c
+++ dialogs/mime-settings/xfce-mime-helper-main.c
@@ -32,6 +32,8 @@ static const gchar *CATEGORY_EXEC_ERRORS[] = {
   N_ ("Failed to execute default Terminal Emulator"),
   N_ ("Failed to execute default Text Editor"),
   N_ ("Failed to execute default Image Viewer"),
+  N_ ("Failed to execute default Audio Player"),
+  N_ ("Failed to execute default Video Player"),
 };
 
 
@@ -132,7 +134,7 @@ main (int argc,
   g_option_context_add_main_entries (opt_ctx, option_entries, NULL);
   g_option_context_set_ignore_unknown_options (opt_ctx, TRUE);
   /* Note to Translators: Do not translate the TYPEs (WebBrowser, MailReader,
-   * FileManager, TerminalEmulator, and ImageViewer),
+   * FileManager, TerminalEmulator, ImageViewer, AudioPlayer, and VideoPlayer), 
    * since the xfce-mime-helper utility will
    * not accept localized TYPEs.
    */
@@ -142,8 +144,10 @@ main (int argc,
                                       "  MailReader       - The preferred Mail Reader.\n"
                                       "  FileManager      - The preferred File Manager.\n"
                                       "  TerminalEmulator - The preferred Terminal Emulator.\n"
-                                      "  TextEditor       - The preferred Text Editor.\n"));
-                                      "  ImageViewer      - The preferred Image Viewer.\n"));
+                                      "  TextEditor       - The preferred Text Editor.\n"
+                                      "  ImageViewer      - The preferred Image Viewer.\n"
+                                      "  AudioPlayer      - The preferred Audio Player.\n"
+                                      "  VideoPlayer      - The preferred Video Player."));
 
   if (!g_option_context_parse (opt_ctx, &argc, &argv, &error))
     {
