xfce-mirror/xfce4-settings

commit 1a318624cb39f7a782985af05f79010f7576a179

display-settings: Always provide an EDID hash
Provide a hash on Wayland as on X11, from the data that the
wlr-output-management protocol dedicates to this purpose (hopefully not
all of them are missing, we'll have to wait and see). On X11, we do what
we can with the data we have, in the absence of a real EDID.

Fixes: #379

common/xfce-randr.c
@@ -557,9 +557,19 @@ xfce_randr_friendly_name (XfceRandr *randr,
     xdisplay = gdk_x11_display_get_xdisplay (randr->priv->display);
     edid_data = xfce_randr_read_edid_data (xdisplay, randr->priv->resources->outputs[output_rr_id]);
 
-    if (edid_data) {
+    if (edid_data)
+    {
         info = decode_edid (edid_data);
         randr->priv->edid[output] = g_compute_checksum_for_data (G_CHECKSUM_SHA1 , edid_data, 128);
+    }
+    else
+    {
+        XRROutputInfo *xinfo = randr->priv->output_info[output];
+        gchar *edid_str = g_strdup_printf ("%s-%lu-%lu-%d-%d-%d",
+                                           xinfo->name, xinfo->mm_width, xinfo->mm_height,
+                                           xinfo->ncrtc, xinfo->nclone, xinfo->nmode);
+        randr->priv->edid[output] = g_compute_checksum_for_string (G_CHECKSUM_SHA1 , edid_str, -1);
+        g_free (edid_str);
     }
 
     /* special case, a laptop */

common/xfce-wlr-output-manager.c
@@ -85,6 +85,7 @@ struct _XfceWlrOutputManager
     _XfceWlrOutputListener listener;
     gpointer listener_data;
     GPtrArray *outputs;
+    gboolean initialized;
 };
 
 static const struct wl_registry_listener registry_listener =
@@ -172,6 +173,7 @@ free_output (gpointer data)
     g_free (output->manufacturer);
     g_free (output->model);
     g_free (output->serial_number);
+    g_free (output->edid);
     g_free (output);
 }
 
@@ -237,6 +239,18 @@ xfce_wlr_output_manager_set_property (GObject *object,
 
 
 static void
+output_make_edid (gpointer data,
+                  gpointer user_data)
+{
+    XfceWlrOutput *output = data;
+    gchar *edid_str = g_strdup_printf ("%s-%s-%s", output->serial_number, output->model, output->manufacturer);
+    output->edid = g_compute_checksum_for_string (G_CHECKSUM_SHA1 , edid_str, -1);
+    g_free (edid_str);
+}
+
+
+
+static void
 xfce_wlr_output_manager_constructed (GObject *object)
 {
     XfceWlrOutputManager *manager = XFCE_WLR_OUTPUT_MANAGER (object);
@@ -254,6 +268,8 @@ xfce_wlr_output_manager_constructed (GObject *object)
         manager->wl_listener->finished = manager_finished;
         zwlr_output_manager_v1_add_listener (manager->wl_manager, manager->wl_listener, manager);
         wl_display_roundtrip (wl_display);
+        g_ptr_array_foreach (manager->outputs, output_make_edid, NULL);
+        manager->initialized = TRUE;
     }
     else
     {
@@ -324,6 +340,11 @@ manager_head (void *data,
     output->scale = wl_fixed_from_double (1.0);
     g_ptr_array_add (manager->outputs, output);
     zwlr_output_head_v1_add_listener (head, &head_listener, output);
+    if (manager->initialized)
+    {
+        wl_display_roundtrip (gdk_wayland_display_get_wl_display (gdk_display_get_default ()));
+        output_make_edid (output, NULL);
+    }
 }
 
 
@@ -610,7 +631,7 @@ xfce_wlr_output_manager_get_display_infos (XfceWlrOutputManager *manager)
     for (guint n = 0; n < manager->outputs->len; n++)
     {
         XfceWlrOutput *output = g_ptr_array_index (manager->outputs, n);
-        display_infos[n] = g_strdup (output->serial_number);
+        display_infos[n] = g_strdup (output->edid);
     }
 
     return display_infos;

common/xfce-wlr-output-manager.h
@@ -53,6 +53,7 @@ typedef struct _XfceWlrOutput
     gchar *manufacturer;
     gchar *model;
     gchar *serial_number;
+    gchar *edid;
 
     /* writable */
     int32_t enabled;

dialogs/display-settings/display-settings-wayland.c
@@ -745,7 +745,7 @@ xfce_display_settings_wayland_save (XfceDisplaySettings *settings,
     g_free (property);
 
     property = g_strdup_printf ("/%s/%s/EDID", scheme, output->name);
-    xfconf_channel_set_string (channel, property, output->serial_number);
+    xfconf_channel_set_string (channel, property, output->edid);
     g_free (property);
 
     /* stop here if output is disabled */

commit f8033f466f09050decbfd1417760054dacec47fb

x11: display-settings: Do not use output name to generate fallback EDID
It may change from one session to another.

Fixes: 1a318624cb39f7a782985af05f79010f7576a179
Related: #528

common/xfce-randr.c
@@ -555,8 +555,8 @@ xfce_randr_friendly_name (XfceRandr *randr,
     else
     {
         XRROutputInfo *xinfo = randr->priv->output_info[output];
-        gchar *edid_str = g_strdup_printf ("%s-%lu-%lu-%d-%d-%d",
-                                           xinfo->name, xinfo->mm_width, xinfo->mm_height,
+        gchar *edid_str = g_strdup_printf ("%lu-%lu-%d-%d-%d",
+                                           xinfo->mm_width, xinfo->mm_height,
                                            xinfo->ncrtc, xinfo->nclone, xinfo->nmode);
         randr->priv->edid[output] = g_compute_checksum_for_string (G_CHECKSUM_SHA1, edid_str, -1);
         g_free (edid_str);