xfce-mirror/xfce4-settings

commit fbd0e50d4f584944ae2d66490ccc0f07fbbdf29d

display-settings: Match by EDID when loading output data from xfconf
The output name may change from one session to another on some systems,
at least on X11, although this seems to be rare.

Fixes: #528

common/xfce-randr.c
@@ -38,6 +38,8 @@ struct _XfceRandrPrivate
     /* cache for the output/mode info */
     XRROutputInfo **output_info;
     XfceRRMode **modes;
+    RROutput *rr_outputs;
+
     /* SHA-1 checksum of the EDID */
     gchar **edid;
 };
@@ -47,7 +49,7 @@ struct _XfceRandrPrivate
 static gchar *
 xfce_randr_friendly_name (XfceRandr *randr,
                           guint output,
-                          guint output_rr_id);
+                          RROutput rr_output);
 
 
 
@@ -154,7 +156,7 @@ xfce_randr_populate (XfceRandr *randr,
     XRRCrtcInfo *crtc_info;
     gint n;
     guint m, connected;
-    guint *output_ids = NULL;
+    RROutput *rr_outputs = NULL;
 
     g_return_if_fail (randr != NULL);
     g_return_if_fail (randr->priv != NULL);
@@ -162,7 +164,7 @@ xfce_randr_populate (XfceRandr *randr,
 
     /* prepare the temporary cache */
     outputs = g_ptr_array_new ();
-    output_ids = g_malloc0 (randr->priv->resources->noutput * sizeof (guint));
+    rr_outputs = g_new0 (RROutput, randr->priv->resources->noutput);
 
     /* walk the outputs */
     connected = 0;
@@ -180,7 +182,7 @@ xfce_randr_populate (XfceRandr *randr,
         }
         else
         {
-            output_ids[connected] = n;
+            rr_outputs[connected] = randr->priv->resources->outputs[n];
             connected++;
         }
 
@@ -191,6 +193,7 @@ xfce_randr_populate (XfceRandr *randr,
     /* migrate the temporary cache */
     randr->noutput = outputs->len;
     randr->priv->output_info = (XRROutputInfo **) g_ptr_array_free (outputs, FALSE);
+    randr->priv->rr_outputs = rr_outputs;
 
     /* allocate final space for the settings */
     randr->mode = g_new0 (RRMode, randr->noutput);
@@ -212,7 +215,7 @@ xfce_randr_populate (XfceRandr *randr,
         randr->priv->modes[m] = xfce_randr_list_supported_modes (randr->priv->resources, randr->priv->output_info[m]);
 
         /* find the primary screen */
-        if (XRRGetOutputPrimary (xdisplay, GDK_WINDOW_XID (root_window)) == randr->priv->resources->outputs[output_ids[m]])
+        if (XRRGetOutputPrimary (xdisplay, GDK_WINDOW_XID (root_window)) == rr_outputs[m])
             randr->status[m] = XFCE_OUTPUT_STATUS_PRIMARY;
         else
             randr->status[m] = XFCE_OUTPUT_STATUS_SECONDARY;
@@ -246,7 +249,7 @@ xfce_randr_populate (XfceRandr *randr,
         }
 
         /* fill in the name used by the UI */
-        randr->friendly_name[m] = xfce_randr_friendly_name (randr, m, output_ids[m]);
+        randr->friendly_name[m] = xfce_randr_friendly_name (randr, m, rr_outputs[m]);
 
         /* Replace spaces with underscore in name for xfconf compatibility */
         g_strcanon (randr->priv->output_info[m]->name,
@@ -254,8 +257,6 @@ xfce_randr_populate (XfceRandr *randr,
     }
     /* populate mirrored details */
     xfce_randr_guess_relations (randr);
-
-    g_free (output_ids);
 }
 
 
@@ -336,6 +337,7 @@ xfce_randr_cleanup (XfceRandr *randr)
     g_free (randr->position);
     g_free (randr->mirrored);
     g_free (randr->priv->output_info);
+    g_free (randr->priv->rr_outputs);
 }
 
 
@@ -535,7 +537,7 @@ xfce_randr_read_edid_data (Display *xdisplay,
 static gchar *
 xfce_randr_friendly_name (XfceRandr *randr,
                           guint output,
-                          guint output_rr_id)
+                          RROutput rr_output)
 {
     Display *xdisplay;
     MonitorInfo *info = NULL;
@@ -545,7 +547,7 @@ xfce_randr_friendly_name (XfceRandr *randr,
 
     /* get the vendor & size */
     xdisplay = gdk_x11_display_get_xdisplay (randr->priv->display);
-    edid_data = xfce_randr_read_edid_data (xdisplay, randr->priv->resources->outputs[output_rr_id]);
+    edid_data = xfce_randr_read_edid_data (xdisplay, rr_output);
 
     if (edid_data)
     {
@@ -711,6 +713,20 @@ xfce_randr_get_edid (XfceRandr *randr,
                      guint noutput)
 {
     return randr->priv->edid[noutput];
+}
+
+
+
+const gchar *
+xfce_randr_get_edid_by_id (XfceRandr *randr,
+                           RROutput output)
+{
+    for (guint n = 0; n < randr->noutput; n++)
+    {
+        if (randr->priv->rr_outputs[n] == output)
+            return randr->priv->edid[n];
+    }
+    return NULL;
 }
 
 

common/xfce-randr.h
@@ -117,6 +117,10 @@ gchar *
 xfce_randr_get_edid (XfceRandr *randr,
                      guint noutput);
 
+const gchar *
+xfce_randr_get_edid_by_id (XfceRandr *randr,
+                           RROutput output);
+
 gchar *
 xfce_randr_get_output_info_name (XfceRandr *randr,
                                  guint noutput);

xfsettingsd/displays-x11.c
@@ -801,27 +801,57 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     Rotation rot;
     gint x, y, n, m, int_value;
     gboolean active;
+    const gchar *edid;
+    gchar *output_name = NULL;
+    GHashTableIter iter;
+    gpointer key;
 
     g_assert (XFCE_IS_DISPLAYS_HELPER (helper) && helper->resources && output);
 
     active = output->active;
 
     /* does this output exist in xfconf? */
-    g_snprintf (property, sizeof (property), OUTPUT_FMT, scheme, output->info->name);
-    value = g_hash_table_lookup (saved_outputs, property);
-
-    if (value == NULL || !G_VALUE_HOLDS_STRING (value))
+    edid = xfce_randr_get_edid_by_id (helper->randr, output->id);
+    if (edid == NULL)
+    {
+        g_warn_if_reached ();
         return active;
+    }
+    g_hash_table_iter_init (&iter, saved_outputs);
+    while (g_hash_table_iter_next (&iter, &key, (gpointer *) &value))
+    {
+        if (g_str_has_suffix (key, "EDID")
+            && G_VALUE_HOLDS_STRING (value)
+            && g_strcmp0 (g_value_get_string (value), edid) == 0)
+        {
+            gchar **tokens = g_strsplit (key, "/", -1);
+            if (g_strv_length (tokens) == 4)
+            {
+                output_name = g_strdup (tokens[2]);
+                if (g_strcmp0 (output_name, output->info->name) != 0)
+                {
+                    xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_OUTPUT_NAMES_MISMATCH,
+                                    output->info->name, output_name, edid);
+                }
+            }
+            g_strfreev (tokens);
+            break;
+        }
+    }
+    if (output_name == NULL)
+    {
+        xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_NO_XFCONF_DATA, output->info->name, edid);
+        return active;
+    }
 
     /* is it the primary output? */
-    g_snprintf (property, sizeof (property), PRIMARY_PROP, scheme,
-                output->info->name);
+    g_snprintf (property, sizeof (property), PRIMARY_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_BOOLEAN (value) && g_value_get_boolean (value))
         helper->primary = output->id;
 
     /* status */
-    g_snprintf (property, sizeof (property), ACTIVE_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), ACTIVE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
 
     if (value == NULL || !G_VALUE_HOLDS_BOOLEAN (value))
@@ -838,8 +868,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         if (crtc->mode != None)
         {
             active = FALSE;
-            xfsettings_dbg (XFSD_DEBUG_DISPLAYS, "%s will be disabled by configuration.",
-                            output->info->name);
+            xfsettings_dbg (XFSD_DEBUG_DISPLAYS, "%s will be disabled by configuration.", output_name);
 
             crtc->mode = None;
             crtc->noutput = 0;
@@ -849,7 +878,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     }
 
     /* rotation */
-    g_snprintf (property, sizeof (property), ROTATION_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), ROTATION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_INT (value))
         int_value = g_value_get_int (value);
@@ -866,7 +895,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     }
 
     /* reflection */
-    g_snprintf (property, sizeof (property), REFLECTION_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), REFLECTION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_STRING (value))
         str_value = g_value_get_string (value);
@@ -884,7 +913,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     /* check rotation support */
     if ((crtc->rotations & rot) == 0)
     {
-        g_warning ("Unsupported rotation for %s. Fallback to RR_Rotate_0.", output->info->name);
+        g_warning ("Unsupported rotation for %s. Fallback to RR_Rotate_0.", output_name);
         rot = RR_Rotate_0;
     }
 
@@ -896,7 +925,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     }
 
     /* resolution */
-    g_snprintf (property, sizeof (property), RESOLUTION_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), RESOLUTION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (value == NULL || !G_VALUE_HOLDS_STRING (value))
         str_value = "";
@@ -904,14 +933,14 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         str_value = g_value_get_string (value);
 
     /* refresh rate */
-    g_snprintf (property, sizeof (property), RRATE_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), RRATE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_DOUBLE (value))
         output_rate = g_value_get_double (value);
     else
         output_rate = 0.0;
 
-    g_snprintf (property, sizeof (property), MODE_FLAGS_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), MODE_FLAGS_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (value == NULL || !G_VALUE_HOLDS_UINT64 (value))
         mode_flags = 0;
@@ -919,7 +948,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         mode_flags = g_value_get_uint64 (value);
 
     /* scaling */
-    g_snprintf (property, sizeof (property), SCALE_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), SCALE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_DOUBLE (value))
         scale = g_value_get_double (value);
@@ -927,7 +956,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         scale = 1.0;
 
     /* backward compatibility; old properties are reset in xfce-randr.c when saving  */
-    g_snprintf (property, sizeof (property), SCALEX_PROP, scheme, output->info->name);
+    g_snprintf (property, sizeof (property), SCALEX_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_DOUBLE (value))
         scale = g_value_get_double (value);
@@ -978,7 +1007,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     if (valid_mode == None)
     {
         /* unsupported mode, abort for this output */
-        g_warning (WARNING_MESSAGE_UNKNOWN_MODE, str_value, output_rate, output->info->name);
+        g_warning (WARNING_MESSAGE_UNKNOWN_MODE, str_value, output_rate, output_name);
         return active;
     }
     else if (crtc->mode != valid_mode)
@@ -1004,8 +1033,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     }
 
     /* position, x */
-    g_snprintf (property, sizeof (property), POSX_PROP, scheme,
-                output->info->name);
+    g_snprintf (property, sizeof (property), POSX_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_INT (value))
         x = g_value_get_int (value);
@@ -1013,8 +1041,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         x = 0;
 
     /* position, y */
-    g_snprintf (property, sizeof (property), POSY_PROP, scheme,
-                output->info->name);
+    g_snprintf (property, sizeof (property), POSY_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     if (G_VALUE_HOLDS_INT (value))
         y = g_value_get_int (value);
@@ -1030,6 +1057,7 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
     }
 
     xfce_displays_helper_x11_set_outputs (crtc, output);
+    g_free (output_name);
 
     return active;
 }

xfsettingsd/displays.h
@@ -53,6 +53,8 @@
 #define DEBUG_MESSAGE_DIFF_N_OUTPUTS "Noutput: before = %d, after = %d."
 #define DEBUG_MESSAGE_ALL_DISABLED "No active output anymore! Attempting to re-enable the internal output."
 #define DEBUG_MESSAGE_NEW_OUTPUT "New output connected: %s"
+#define DEBUG_MESSAGE_OUTPUT_NAMES_MISMATCH "Output names don't match: '%s' (system) loaded as '%s' (xfconf), EDID '%s'"
+#define DEBUG_MESSAGE_NO_XFCONF_DATA "No xfconf data found for output '%s', EDID '%s'"
 
 G_BEGIN_DECLS
 

xfsettingsd/displays-wayland.c
@@ -125,19 +125,45 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
                   GHashTable *saved_outputs,
                   XfceWlrOutput *output)
 {
-    gchar *property = g_strdup_printf (OUTPUT_FMT, scheme, output->name);
-    GValue *value = g_hash_table_lookup (saved_outputs, property);
+    gchar *property;
+    GValue *value;
     GList *lp;
     const gchar *str_value;
     gint int_value;
     gdouble double_value;
-    g_free (property);
+    gchar *output_name = NULL;
+    GHashTableIter iter;
+    gpointer key;
 
-    if (!G_VALUE_HOLDS_STRING (value))
+    g_hash_table_iter_init (&iter, saved_outputs);
+    while (g_hash_table_iter_next (&iter, &key, (gpointer *) &value))
+    {
+        if (g_str_has_suffix (key, "EDID")
+            && G_VALUE_HOLDS_STRING (value)
+            && g_strcmp0 (g_value_get_string (value), output->edid) == 0)
+        {
+            gchar **tokens = g_strsplit (key, "/", -1);
+            if (g_strv_length (tokens) == 4)
+            {
+                output_name = g_strdup (tokens[2]);
+                if (g_strcmp0 (output_name, output->name) != 0)
+                {
+                    xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_OUTPUT_NAMES_MISMATCH,
+                                    output->name, output_name, output->edid);
+                }
+            }
+            g_strfreev (tokens);
+            break;
+        }
+    }
+    if (output_name == NULL)
+    {
+        xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_NO_XFCONF_DATA, output->name, output->edid);
         return;
+    }
 
     /* status */
-    property = g_strdup_printf (ACTIVE_PROP, scheme, output->name);
+    property = g_strdup_printf (ACTIVE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (!G_VALUE_HOLDS_BOOLEAN (value))
@@ -148,7 +174,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
         return;
 
     /* resolution */
-    property = g_strdup_printf (RESOLUTION_PROP, scheme, output->name);
+    property = g_strdup_printf (RESOLUTION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_STRING (value))
@@ -157,7 +183,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
         str_value = "";
 
     /* refresh rate */
-    property = g_strdup_printf (RRATE_PROP, scheme, output->name);
+    property = g_strdup_printf (RRATE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_DOUBLE (value))
@@ -179,7 +205,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
     if (lp == NULL)
     {
         /* unsupported mode, abort for this output */
-        g_warning (WARNING_MESSAGE_UNKNOWN_MODE, str_value, double_value, output->name);
+        g_warning (WARNING_MESSAGE_UNKNOWN_MODE, str_value, double_value, output_name);
         return;
     }
     else
@@ -189,7 +215,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
     }
 
     /* rotation */
-    property = g_strdup_printf (ROTATION_PROP, scheme, output->name);
+    property = g_strdup_printf (ROTATION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_INT (value))
@@ -198,7 +224,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
         int_value = 0;
 
     /* reflection */
-    property = g_strdup_printf (REFLECTION_PROP, scheme, output->name);
+    property = g_strdup_printf (REFLECTION_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_STRING (value))
@@ -249,7 +275,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
     }
 
     /* scaling */
-    property = g_strdup_printf (SCALE_PROP, scheme, output->name);
+    property = g_strdup_printf (SCALE_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_DOUBLE (value))
@@ -260,7 +286,7 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
     output->scale = wl_fixed_from_double (double_value);
 
     /* position, x */
-    property = g_strdup_printf (POSX_PROP, scheme, output->name);
+    property = g_strdup_printf (POSX_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_INT (value))
@@ -269,13 +295,15 @@ load_from_xfconf (XfceDisplaysHelperWayland *helper,
         output->x = 0;
 
     /* position, y */
-    property = g_strdup_printf (POSY_PROP, scheme, output->name);
+    property = g_strdup_printf (POSY_PROP, scheme, output_name);
     value = g_hash_table_lookup (saved_outputs, property);
     g_free (property);
     if (G_VALUE_HOLDS_INT (value))
         output->y = g_value_get_int (value);
     else
         output->y = 0;
+
+    g_free (output_name);
 }
 
 

commit c83a43fe9f049fd58a4159de6c55a4d268e72228

x11: display-settings: Fall back to output name when EDID is duplicated
Unfortunately, this can happen on X11, but it seems less likely on
Wayland, so there's no need to weigh down the code unnecessarily. In
this case, the only option is to fall back on the output name as before
fbd0e50d, hoping that it will be stable from one session to the next,
otherwise we will encounter bug #528 again without being able to do
anything about it.

Fixes: fbd0e50d4f584944ae2d66490ccc0f07fbbdf29d
Closes: #628

common/xfce-randr.c
@@ -384,7 +384,8 @@ void
 xfce_randr_save_output (XfceRandr *randr,
                         const gchar *scheme,
                         XfconfChannel *channel,
-                        guint output)
+                        guint output,
+                        gboolean duplicate)
 {
     gchar property[512];
     gchar *str_value;
@@ -411,6 +412,9 @@ xfce_randr_save_output (XfceRandr *randr,
     g_snprintf (property, sizeof (property), "/%s/%s/EDID", scheme,
                 randr->priv->output_info[output]->name);
     xfconf_channel_set_string (channel, property, randr->priv->edid[output]);
+    g_snprintf (property, sizeof (property), "/%s/%s/DuplicateEDID", scheme,
+                randr->priv->output_info[output]->name);
+    xfconf_channel_set_bool (channel, property, duplicate);
 
     if (mode == NULL)
         return;

common/xfce-randr.h
@@ -90,7 +90,8 @@ void
 xfce_randr_save_output (XfceRandr *randr,
                         const gchar *scheme,
                         XfconfChannel *channel,
-                        guint output);
+                        guint output,
+                        gboolean duplicate);
 
 void
 xfce_randr_load (XfceRandr *randr,

dialogs/display-settings/display-settings-x11.c
@@ -572,10 +572,20 @@ xfce_display_settings_x11_save (XfceDisplaySettings *settings,
 {
     XfceRandr *randr = XFCE_DISPLAY_SETTINGS_X11 (settings)->randr;
     XfconfChannel *channel = xfce_display_settings_get_channel (settings);
+    GHashTable *edids = g_hash_table_new (g_str_hash, g_str_equal);
     for (guint n = 0; n < randr->noutput; n++)
     {
-        xfce_randr_save_output (randr, scheme, channel, n);
+        const gchar *edid = xfce_randr_get_edid (randr, n);
+        gpointer duplicate = GINT_TO_POINTER (g_hash_table_contains (edids, edid));
+        g_hash_table_insert (edids, (gpointer) edid, duplicate);
     }
+    for (guint n = 0; n < randr->noutput; n++)
+    {
+        const gchar *edid = xfce_randr_get_edid (randr, n);
+        gboolean duplicate = GPOINTER_TO_INT (g_hash_table_lookup (edids, edid));
+        xfce_randr_save_output (randr, scheme, channel, n, duplicate);
+    }
+    g_hash_table_destroy (edids);
 }
 
 

xfsettingsd/displays-x11.c
@@ -817,25 +817,39 @@ xfce_displays_helper_x11_load_from_xfconf (XfceDisplaysHelperX11 *helper,
         g_warn_if_reached ();
         return active;
     }
-    g_hash_table_iter_init (&iter, saved_outputs);
-    while (g_hash_table_iter_next (&iter, &key, (gpointer *) &value))
+
+    /* if this output EDID is duplicated in scheme, we fall back to matching by name */
+    g_snprintf (property, sizeof (property), DUPLICATE_EDID_PROP, scheme, output->info->name);
+    value = g_hash_table_lookup (saved_outputs, property);
+    if (G_VALUE_HOLDS_BOOLEAN (value) && g_value_get_boolean (value))
     {
-        if (g_str_has_suffix (key, "EDID")
-            && G_VALUE_HOLDS_STRING (value)
-            && g_strcmp0 (g_value_get_string (value), edid) == 0)
+        output_name = g_strdup (output->info->name);
+        xfsettings_dbg (XFSD_DEBUG_DISPLAYS,
+                        "EDID '%s' of output '%s' is duplicated in profile '%s': matching by name instead",
+                        edid, output->info->name, scheme);
+    }
+    else
+    {
+        g_hash_table_iter_init (&iter, saved_outputs);
+        while (g_hash_table_iter_next (&iter, &key, (gpointer *) &value))
         {
-            gchar **tokens = g_strsplit (key, "/", -1);
-            if (g_strv_length (tokens) == 4)
+            if (g_str_has_suffix (key, "EDID")
+                && G_VALUE_HOLDS_STRING (value)
+                && g_strcmp0 (g_value_get_string (value), edid) == 0)
             {
-                output_name = g_strdup (tokens[2]);
-                if (g_strcmp0 (output_name, output->info->name) != 0)
+                gchar **tokens = g_strsplit (key, "/", -1);
+                if (g_strv_length (tokens) == 4)
                 {
-                    xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_OUTPUT_NAMES_MISMATCH,
-                                    output->info->name, output_name, edid);
+                    output_name = g_strdup (tokens[2]);
+                    if (g_strcmp0 (output_name, output->info->name) != 0)
+                    {
+                        xfsettings_dbg (XFSD_DEBUG_DISPLAYS, DEBUG_MESSAGE_OUTPUT_NAMES_MISMATCH,
+                                        output->info->name, output_name, edid);
+                    }
                 }
+                g_strfreev (tokens);
+                break;
             }
-            g_strfreev (tokens);
-            break;
         }
     }
     if (output_name == NULL)

xfsettingsd/displays.h
@@ -29,6 +29,7 @@
 #define ACTIVE_PROFILE "/ActiveProfile"
 #define AUTO_ENABLE_PROFILES "/AutoEnableProfiles"
 #define OUTPUT_FMT "/%s/%s"
+#define DUPLICATE_EDID_PROP OUTPUT_FMT "/DuplicateEDID"
 #define PRIMARY_PROP OUTPUT_FMT "/Primary"
 #define ACTIVE_PROP OUTPUT_FMT "/Active"
 #define ROTATION_PROP OUTPUT_FMT "/Rotation"