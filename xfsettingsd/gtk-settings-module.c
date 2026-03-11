ce6a47a503a5298b4b44ed70d12e1151cb1fecbb
Brian J. Tarricone <brian@tarricone.org>
2026-03-11 01:59:17 -0700

    Include an allowlist of xfconf props to sync to GtkSettings
    Notably, we do *not* sync /Xft/DPI on Wayland, as GTK figures out DPI
    based on wl_output (of which there may be several, with different DPIs).
    The Appearance Settings dialog hides this setting on Wayland, so we
    shouldn't override it with something the user can't reasonably set
    anyway.
    
    Additionally, with the previous commit, if we were still syncing
    /Xft/DPI, it would make text disappear, because the DPI value needs to
    be multiplied by 1024 before setting it on GtkSettings, which we weren't
    doing.
    
    The sync_properties list also leaves out two or three xfconf properties
    that do not have corresponding GtkSettings properties.
    
    Closes #636

--- xfsettingsd/gtk-settings-module.c
+++ xfsettingsd/gtk-settings-module.c
@@ -29,6 +29,37 @@ gtk_module_init (gint *argc,
 G_MODULE_EXPORT const gchar *
 g_module_check_init (GModule *module);
 
+static const gchar *sync_properties[] = {
+    "/Gtk/ButtonImages",
+    "/Gtk/CanChangeAccels",
+    "/Gtk/ColorPalette",
+    "/Gtk/CursorThemeName",
+    "/Gtk/CursorThemeSize",
+    "/Gtk/DecorationLayout",
+    "/Gtk/DialogsUseHeader",
+    "/Gtk/FontName",
+    "/Gtk/IconSizes",
+    "/Gtk/KeyThemeName",
+    "/Gtk/MenuBarAccel",
+    "/Gtk/MenuImages",
+    "/Gtk/Modules",
+    "/Gtk/TitlebarMiddleClick",
+    "/Net/CursorBlink",
+    "/Net/CursorBlinkTime",
+    "/Net/DndDragThreshold",
+    "/Net/DoubleClickDistance",
+    "/Net/DoubleClickTime",
+    "/Net/EnableEventSounds",
+    "/Net/EnableInputFeedbackSounds",
+    "/Net/IconThemeName",
+    "/Net/SoundThemeName",
+    "/Net/ThemeName",
+    "/Xft/Antialias",
+    "/Xft/HintStyle",
+    "/Xft/Hinting",
+    "/Xft/RGBA",
+};
+
 
 
 /*
@@ -70,37 +101,51 @@ xfconf_prop_to_gtk_setting (const gchar *prop)
 
 
 
+static gboolean
+should_sync_property (const gchar *property)
+{
+    for (gsize i = 0; i < G_N_ELEMENTS (sync_properties); ++i)
+        if (strcmp (property, sync_properties[i]) == 0)
+            return TRUE;
+    return FALSE;
+}
+
+
+
 static void
 property_changed (XfconfChannel *channel,
                   const gchar *property,
                   const GValue *value,
                   gpointer data)
 {
-    GtkSettings *settings = gtk_settings_get_default ();
-    gchar *setting = xfconf_prop_to_gtk_setting (property);
-    GParamSpec *pspec = g_object_class_find_property (G_OBJECT_GET_CLASS (settings), setting);
-
-    if (pspec != NULL)
+    if (should_sync_property (property))
     {
-        const GValue *default_value = g_param_spec_get_default_value (pspec);
+        GtkSettings *settings = gtk_settings_get_default ();
+        gchar *setting = xfconf_prop_to_gtk_setting (property);
+        GParamSpec *pspec = g_object_class_find_property (G_OBJECT_GET_CLASS (settings), setting);
 
-        xfsettings_dbg (XFSD_DEBUG_GTK_SETTINGS,
-                        "Xfconf property '%s' changed, syncing with GtkSettings property '%s'",
-                        property, g_param_spec_get_name (pspec));
-
-        if (G_VALUE_TYPE (value) == G_TYPE_INVALID)
-            g_object_set_property (G_OBJECT (settings), setting, default_value);
-        else
+        if (pspec != NULL)
         {
-            GValue trans_value = G_VALUE_INIT;
-            g_value_init (&trans_value, G_VALUE_TYPE (default_value));
-            g_value_transform (value, &trans_value);
-            g_object_set_property (G_OBJECT (settings), setting, &trans_value);
-            g_value_unset (&trans_value);
-        }
-    }
+            const GValue *default_value = g_param_spec_get_default_value (pspec);
 
-    g_free (setting);
+            xfsettings_dbg (XFSD_DEBUG_GTK_SETTINGS,
+                            "Xfconf property '%s' changed, syncing with GtkSettings property '%s'",
+                            property, g_param_spec_get_name (pspec));
+
+            if (G_VALUE_TYPE (value) == G_TYPE_INVALID)
+                g_object_set_property (G_OBJECT (settings), setting, default_value);
+            else
+            {
+                GValue trans_value = G_VALUE_INIT;
+                g_value_init (&trans_value, G_VALUE_TYPE (default_value));
+                g_value_transform (value, &trans_value);
+                g_object_set_property (G_OBJECT (settings), setting, &trans_value);
+                g_value_unset (&trans_value);
+            }
+        }
+
+        g_free (setting);
+    }
 }
 
 
@@ -121,7 +166,8 @@ gtk_module_init (gint *argc,
     props = xfconf_channel_get_properties (channel, NULL);
     g_hash_table_iter_init (&iter, props);
     while (g_hash_table_iter_next (&iter, (gpointer *) &prop, (gpointer *) &value))
-        property_changed (channel, prop, value, NULL);
+        if (should_sync_property (prop))
+            property_changed (channel, prop, value, NULL);
 
     g_hash_table_destroy (props);
 }
